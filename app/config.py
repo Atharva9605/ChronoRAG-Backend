86c5ebc601ace02d656f6c8255e7cc488ce2947a
AnshulParate2004 <anshulnparate@gmail.com>
2026-08-22 20:35:02 +0530

    fix: app/config.py ROOT path resolution

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-ROOT = Path(__file__).resolve().parents[2]
+ROOT = Path(__file__).resolve().parents[1]
 
 
 class Settings(BaseSettings):
